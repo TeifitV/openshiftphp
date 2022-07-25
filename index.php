3c47e2645c4c25f9588e7dd21ef52d823ee87335
TeifitV <teifitv@gmail.com>
2022-07-25 10:38:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	</head>
 
 <body> 
-	<h1>PHP 2022  Test</h1>
-		<p><b>AmdocsNA Session</b></p>
+	<h1>PHP 2022  Test TOMAS VARGAS</h1>
+		<p><b>AmdocsNA Session 4</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information </h2> 
